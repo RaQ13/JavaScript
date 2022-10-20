0f0718fd64c9869559c62ee7d7d3aef720e4f4d8
RaQ13 <lukasz46@op.pl>
2022-10-20 10:04:25 +0200

    used interval and timeout for console logs

--- 01_Dzien_1/01_Funkcje_czasu/01_Zadanie_1/js/app.js
+++ 01_Dzien_1/01_Funkcje_czasu/01_Zadanie_1/js/app.js
@@ -1,1 +1,7 @@
+const intervalID = setInterval(function () {
+    console.log("Wygenerowano z setInterval");
+}, 10000);
 
+const timeoutID = setTimeout(function (){
+    console.log("JavaScript Rules");
+}, 4000);
