3ff3bb71626ad1f38d6297c5752e08d56fd316a4
RaQ13 <lukasz46@op.pl>
2022-10-20 11:39:11 +0200

    hoisting in js

--- 01_Dzien_1/02_Wiecej_o_funkcjach/03_Zadanie_3/js/app.js
+++ 01_Dzien_1/02_Wiecej_o_funkcjach/03_Zadanie_3/js/app.js
@@ -1,2 +1,12 @@
+sayHello1(); //można wywołać funkcje przed deklaracją
+function sayHello1(){
+    console.log("Cześć");
+}
+sayHello1(); // można wywołać funkcje po deklaracji
 
-
+// witaj(); nie można wywołąć przed deklaracja,
+//hoisting nie działa z wyrażeniami funkcyjnymi
+const witaj = function () {
+    console.log("Witaj");
+}
+witaj(); // można wywołać po deklaracji
