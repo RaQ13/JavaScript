038fd380fc75c38b5d9e4202c02ae80d0e38c4a4
RaQ13 <lukasz46@op.pl>
2022-10-20 10:23:30 +0200

    interval used for print hello with couner as many times as definied in hello variable

--- 01_Dzien_1/01_Funkcje_czasu/02_Zadanie_2/js/app.js
+++ 01_Dzien_1/01_Funkcje_czasu/02_Zadanie_2/js/app.js
@@ -1,1 +1,14 @@
+const hello = 7;
 
+function countHello(limit){
+    let counter = 0;
+    const helloCounterInterval = setInterval(function (){
+       console.log("Hello " + (counter+1));
+       counter++;
+       if (counter === limit) {
+           clearInterval(helloCounterInterval);
+       }
+    },2000);
+}
+
+countHello(hello);
