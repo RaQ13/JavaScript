29f20ddc20aefd1b8878244d0ccc1bc0ee639d06
RaQ13 <lukasz46@op.pl>
2022-10-21 09:48:08 +0200

    function distFromAverage is counting average number from array and returns an array with difference between average number and numbers from array.

--- 01_Dzien_1/03_Tablice/01_Zadanie_1/js/app.js
+++ 01_Dzien_1/03_Tablice/01_Zadanie_1/js/app.js
@@ -1,1 +1,18 @@
+array = [1,2,3,4,5,6,7];
 
+let average = 0;
+
+function distFromAverage (array) {
+    array.forEach(function (el){
+        average += el;
+    });
+    average = (Math.floor(average / array.length));
+    const dist = array.map(function (el){
+       return average - el
+    });
+
+    console.log(dist);
+    console.log(average);
+}
+
+distFromAverage(array);
