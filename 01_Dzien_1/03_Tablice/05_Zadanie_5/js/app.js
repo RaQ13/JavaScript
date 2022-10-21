329cb90a83966afb1b23802b456912fe887c79d8
RaQ13 <lukasz46@op.pl>
2022-10-21 11:59:46 +0200

    function multiply mulpitlies every elements in array

--- 01_Dzien_1/03_Tablice/05_Zadanie_5/js/app.js
+++ 01_Dzien_1/03_Tablice/05_Zadanie_5/js/app.js
@@ -1,1 +1,14 @@
+const array = [1,2,3,4,5,6,7];
+const array2 = [2,8,3,7];
 
+function multiply(array){
+    let result = array[0];
+    array.forEach(function (el, index){
+        if (index > 0) {
+            result = result * el
+        }
+
+    });
+    console.log(result);
+}
+multiply(array);
