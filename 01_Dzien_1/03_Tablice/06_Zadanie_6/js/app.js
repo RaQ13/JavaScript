3af2d79d05801b31f4d2d4d1894d80f86884b3a1
RaQ13 <lukasz46@op.pl>
2022-10-27 08:08:18 +0200

    function getEvenAverage is getting an average number from even numbers of entered array

--- 01_Dzien_1/03_Tablice/06_Zadanie_6/js/app.js
+++ 01_Dzien_1/03_Tablice/06_Zadanie_6/js/app.js
@@ -1,1 +1,24 @@
+// const array = [1,2,3,4,5,6,7];
+// const array = [1,1,1,1];
+const array = [2,8,3,7,4];
 
+function getEvenAverage(array) {
+    const arrayEven = [];
+    let average = 0;
+    let evenAverage = 0;
+    array.forEach(function (el){
+        if (el % 2 === 0) {
+            arrayEven.push(el);
+        }
+    });
+    if (arrayEven.length === 0) {
+        return null;
+    }
+    arrayEven.forEach(function (el){
+        average += el;
+    });
+    evenAverage = (average / arrayEven.length).toFixed(2);
+    console.log(evenAverage);
+}
+
+getEvenAverage(array);
