a9eaf469c83e59114b4bffd862f93be75f0fd797
RaQ13 <lukasz46@op.pl>
2022-10-27 08:14:49 +0200

    function sortArray is sorting in ascending order numbers from array

--- 01_Dzien_1/03_Tablice/07_Zadanie_7/js/app.js
+++ 01_Dzien_1/03_Tablice/07_Zadanie_7/js/app.js
@@ -1,1 +1,10 @@
+const array = [145,11,3,64,4,6,10];
 
+function sortArray(array){
+    const sorted = array.sort(function (a , b){
+       return a - b;
+    });
+    console.log(sorted);
+}
+
+sortArray(array);
