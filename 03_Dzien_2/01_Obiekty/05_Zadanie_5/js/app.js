0a8be1bfca61c4babca0aa7f7a1f3509423234c1
RaQ13 <lukasz46@op.pl>
2022-11-15 09:39:04 +0100

    object recipe is created and ingridients value is added outside object

---
+++ 03_Dzien_2/01_Obiekty/05_Zadanie_5/js/app.js
@@ -0,0 +1,9 @@
+const recipe = {
+    title: "naleśnik",
+    servings: 8
+}
+recipe.ingridients = ["2 jajka", "szklanka mleka", "szklanka mąki"];
+
+console.log(recipe.title);
+console.log(recipe.servings);
+console.log(recipe.ingridients);
