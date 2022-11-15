beea9ee6c8e153010c16477db335eec9560617db
RaQ13 <lukasz46@op.pl>
2022-11-15 09:27:39 +0100

    object book is created

---
+++ 03_Dzien_2/01_Obiekty/03_Zadanie_3/js/app.js
@@ -0,0 +1,9 @@
+const book = {
+    title: "Harry Potter and Halfblood Prince",
+    author: "J.K. Rowling",
+    numberOfPages: 704
+}
+
+console.log(book.title);
+console.log(book.author);
+console.log(book.numberOfPages);
