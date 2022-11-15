5a71958c496597e1138953d156cfa6a1975bc337
RaQ13 <lukasz46@op.pl>
2022-11-15 09:04:03 +0100

    object capital is created and values are printed

---
+++ 03_Dzien_2/01_Obiekty/01_Zadanie_1/js/app.js
@@ -0,0 +1,11 @@
+const country = {
+    capial: "stolica",
+    population: 256626,
+    president: "Dudeł",
+    ministers: ["pierwszy", "drugi", "jakiś-inny"]
+}
+
+console.log(country.capial);
+console.log(country.population);
+console.log(country.president);
+console.log(country.ministers);
