169fa5edebc2c4b890e05d7db0b5d1a464c00440
RaQ13 <lukasz46@op.pl>
2022-11-17 09:01:57 +0100

    all paragrafs in nav element with navigation class, all paragraphs in all divs and all divs in article are printed

--- 03_Dzien_2/04_Wyszukiwanie_elementow/04_Zadanie_4/js/app.js
+++ 03_Dzien_2/04_Wyszukiwanie_elementow/04_Zadanie_4/js/app.js
@@ -1,1 +1,6 @@
-
+const allLiInNavNavigation = document.querySelectorAll('nav.navigation li');
+console.log(allLiInNavNavigation);
+const allPInAllDivs = document.querySelectorAll('div p');
+console.log(allPInAllDivs);
+const allDivsInArticle = document.querySelectorAll('article div');
+console.log(allDivsInArticle);
