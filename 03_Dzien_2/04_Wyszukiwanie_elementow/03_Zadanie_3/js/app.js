cc1a3d64819e50b2e97fe98bef0f1ffeda8d7c45
RaQ13 <lukasz46@op.pl>
2022-11-17 08:46:48 +0100

    element with id home in 2 ways, first li element with attribute data-direction and first element with block class are printed

--- 03_Dzien_2/04_Wyszukiwanie_elementow/03_Zadanie_3/js/app.js
+++ 03_Dzien_2/04_Wyszukiwanie_elementow/03_Zadanie_3/js/app.js
@@ -1,1 +1,8 @@
-
+const idByHash = document.querySelector('#home');
+console.log(idByHash);
+const idByGetElementBy = document.getElementById('home');
+console.log(idByGetElementBy);
+const liWithDataDirection = document.querySelector("li[data-direction]");
+console.log(liWithDataDirection);
+const block = document.querySelector('.block');
+console.log(block);
