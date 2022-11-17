8aedaf87729a7b9bcbaba05d0a05b82944e911df
RaQ13 <lukasz46@op.pl>
2022-11-17 10:45:50 +0100

    innerText is used for getting and setting values

--- 03_Dzien_2/05_Elementy/03_Zadanie_3/js/app.js
+++ 03_Dzien_2/05_Elementy/03_Zadanie_3/js/app.js
@@ -1,1 +1,9 @@
 const blocks = document.querySelectorAll(".block");
+console.log(blocks);
+
+Array.from(blocks).forEach(function (el){
+   console.log(el.innerText);
+   el.innerText = "Nowa wartość diva o klasie blocks";
+   console.log(el.innerText);
+});
+
