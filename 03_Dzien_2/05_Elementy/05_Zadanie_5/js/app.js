cadd4be6c8fd24d90a80f09bf3c9e69c1509ed11
RaQ13 <lukasz46@op.pl>
2022-11-17 11:00:40 +0100

    function get tags is getting tags from elements

--- 03_Dzien_2/05_Elementy/05_Zadanie_5/js/app.js
+++ 03_Dzien_2/05_Elementy/05_Zadanie_5/js/app.js
@@ -1,1 +1,11 @@
 const childElements = document.querySelector(".offers").querySelectorAll("div, h2, p");
+
+function getTags(elements) {
+    let array = [];
+    Array.from(elements).forEach(function (el){
+        array.push(el.tagName);
+    })
+    return array;
+}
+
+console.log(getTags(childElements));
