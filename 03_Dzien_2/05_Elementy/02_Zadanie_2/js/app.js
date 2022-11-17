6b5c335f5e35b45121c5aa6051980e2c7d2a52ff
RaQ13 <lukasz46@op.pl>
2022-11-17 10:26:54 +0100

    fot loop is used in node arrays for printing the tags and classes of elements

--- 03_Dzien_2/05_Elementy/02_Zadanie_2/js/app.js
+++ 03_Dzien_2/05_Elementy/02_Zadanie_2/js/app.js
@@ -3,3 +3,22 @@ const children = document.querySelector(".offers").querySelectorAll("div, h2, p"
 const ban = document.querySelector(".ban");
 const someBlocks = document.querySelectorAll(".block");
 const listElements = document.querySelector(".links").querySelectorAll("li");
+
+console.log(home);
+console.log(children); //node
+console.log(ban);
+console.log(someBlocks); //node
+console.log(listElements); //node
+
+function forNode (nodearray) {
+    for (let i = 0; i < nodearray.length; i++) {
+        console.log("tagi = " + nodearray[i].nodeName);
+        Array.from(nodearray[i].classList).forEach(function (el){
+            console.log("klasy = " + el);
+        });
+    }
+}
+
+forNode(children);
+forNode(someBlocks);
+forNode(listElements);
