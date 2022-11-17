835ae6b678f1565d1440e1143ef40abbf1c6f5f7
RaQ13 <lukasz46@op.pl>
2022-11-17 09:39:00 +0100

    function getDataInfo is filling an array with values from data-color of links

--- 03_Dzien_2/05_Elementy/01_Zadanie_1/js/app.js
+++ 03_Dzien_2/05_Elementy/01_Zadanie_1/js/app.js
@@ -1,1 +1,11 @@
 const links = document.querySelector(".links").querySelectorAll("li");
+
+function getDataInfo(elements) {
+    let array = [];
+    Array.from(elements).forEach(function (el){
+        array.push(el.attributes[0].nodeValue);
+    });
+    return array;
+}
+
+console.log(getDataInfo(links));
