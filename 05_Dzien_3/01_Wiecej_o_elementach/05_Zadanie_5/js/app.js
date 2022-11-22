78b60c6d36ca3a1e991b8bc9e6078e8c597e90d4
RaQ13 <lukasz46@op.pl>
2022-11-22 08:27:57 +0100

    all element have data-id attribute with growing value

--- 05_Dzien_3/01_Wiecej_o_elementach/05_Zadanie_5/js/app.js
+++ 05_Dzien_3/01_Wiecej_o_elementach/05_Zadanie_5/js/app.js
@@ -1,3 +1,9 @@
 /*
   Poniżej napisz kod rozwiązujący zadania
  */
+let dataID = 1;
+const elements = document.querySelectorAll(".exercise ul li");
+Array.from(elements).forEach(function (el){
+   el.dataset.id = dataID;
+   dataID += 1;
+});
