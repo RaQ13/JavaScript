e222cfd305198a9bc490fd3318d76b487321fde8
RaQ13 <lukasz46@op.pl>
2022-11-22 08:11:26 +0100

    added and remover classes from menu with classlist

--- 05_Dzien_3/01_Wiecej_o_elementach/04_Zadanie_4/js/app.js
+++ 05_Dzien_3/01_Wiecej_o_elementach/04_Zadanie_4/js/app.js
@@ -1,3 +1,11 @@
 /*
   Poniżej napisz kod rozwiązujący zadania
  */
+const ul = document.querySelector(".exercise ul");
+ul.classList.add("menu");
+
+const li = document.querySelectorAll(".exercise ul li");
+Array.from(li).forEach(function (el){
+   el.classList.add("menuElement");
+   el.classList.remove("error");
+});
