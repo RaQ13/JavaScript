1f9b6e21eafd66befe60f1e6e14a5c9a19647dfa
RaQ13 <lukasz46@op.pl>
2022-11-22 08:06:34 +0100

    li element filled with info with innerText

--- 05_Dzien_3/01_Wiecej_o_elementach/03_Zadanie_3/js/app.js
+++ 05_Dzien_3/01_Wiecej_o_elementach/03_Zadanie_3/js/app.js
@@ -1,3 +1,8 @@
 /*
   Poniżej napisz kod rozwiązujący zadania
  */
+const name = document.querySelector(".exercise ul li");
+
+name.innerText += " Erwin Kokorycz";
+name.nextElementSibling.innerText += " Czarny";
+name.nextElementSibling.nextElementSibling.innerText += " płatki";
